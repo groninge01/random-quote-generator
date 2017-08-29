5422bc0e9590b0ee3a47d89dae999329b83f916b
Stefan van Groningen <groninge@hotmail.com>
2017-08-29 21:46:14 +0200

    Moved components to app/components

---
+++ src/app/components/random-quote/random-quote.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Quote } from '../../classes/quote';
+import { QuoteService } from '../../services/quote.service';
+
+@Component({
+  selector: 'app-randomquote',
+  templateUrl: './random-quote.component.html',
+  styleUrls: ['./random-quote.component.css']
+})
+export class RandomQuoteComponent implements OnInit {
+  quote: Quote[];
+
+  constructor(
+    private quoteService: QuoteService,
+  ) { }
+
+  getQuote(): void {
+    this.quoteService.getQuote().then(quote => this.quote = quote);
+  }
+
+  ngOnInit(): void {
+    this.getQuote();
+  }
+}
